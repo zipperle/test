538806a7ba5da530f694f4d66311c2aaefb431fb
Florian Zipperle <dev@zipperle.net>
2016-10-14 13:24:25 +0200

    bug fix

--- main.java
+++ main.java
@@ -23,7 +23,7 @@ public class Main{
         } else if (n == 1) {
             return 1;
         } else {
-            return fib(n-1) + fib(n-1);
+            return fib(n-1) + fib(n-2);
         }
     }
 }
