6e2a1c7b4807ebf05d7b6ad7bcb1fa08ae01c7dc
Florian Zipperle <dev@zipperle.net>
2016-10-14 13:24:08 +0200

    init

---
+++ main.java
@@ -0,0 +1,29 @@
+package test;
+
+/** Main */
+public class Main{ 
+    
+    /** main */
+	public static void main(String [] args){
+		int a = 10;
+        int b = 5;
+        int c = div(a, b);
+        System.out.println(c);
+	}
+
+    /** div */
+    private static int div(int a, int b){
+        return a/b;
+    }
+
+    /** fib */
+    private static int fib(int n){
+        if(n == 0) {
+             return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fib(n-1) + fib(n-1);
+        }
+    }
+}
