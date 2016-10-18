e9d0466c8ba31d2525d24d2cb924f4ed8a167a19
Florian Zipperle <zipperle@cqse.eu>
2016-10-18 14:10:36 +0200

    nesting depth

--- main.java
+++ main.java
@@ -20,7 +20,9 @@ public class Main{
     /** fib */
     private static int fib(int n){
         if(n == 0) {
-             return 0;
+            if(n == 0){
+                return 0;
+            }
         } else if (n == 1) {
             return 1;
         } else {
